8e7b7d542e031addbc6092698b813b98bd3f3b65
Jesse Harlan <jesse.harlan@centralia.edu>
2024-10-16 10:06:32 -0700

    Add project files.

---
+++ GroceryListExample/Item.cs
@@ -0,0 +1,26 @@
+﻿namespace GroceryListExample
+{
+    public class Item
+    {
+        // attributes or properties - things we know
+        public string Name;
+        public double Price;
+        private double Cost;
+
+        public Item()
+        {
+        
+        }
+
+        // method (functions) - things we can do
+        public void calculateCost()
+        {
+            Console.WriteLine("Calculating Cost");
+        }
+
+        public double getCost()
+        {
+            return Cost;
+        }
+    }
+}
---
+++ GroceryListExample/Program.cs
@@ -0,0 +1,17 @@
+﻿// Jesse Harlan
+
+using GroceryListExample;
+
+Item toast = new Item();
+toast.Name = "Yeah Toast";
+
+Item peanutButter = new Item();
+peanutButter.Name = "Peanut Butter Mayhem";
+
+Item other = toast;
+other.Name = "Other Toast";
+
+Console.WriteLine("Other: " + other.Name);
+Console.WriteLine("Toast: " + toast.Name);
+Console.WriteLine("PeanutButter: " + peanutButter.Name);
+
